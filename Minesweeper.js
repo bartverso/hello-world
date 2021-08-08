93444bb3239573b9b78e2db694a0d2f51c6fe80c
Bart Verschooren <bart.verschooren@gmail.com>
2021-08-08 23:31:11 +0200

    Started working on Minesweeper

--- Minesweeper.js
+++ Minesweeper.js
@@ -5,9 +5,31 @@
 
 // Declarations
 
+var PlayingBord = {
+    "A" : [0,0,0,0,0,0,0,0,0,0],
+    "B" : [0,0,0,0,0,0,0,0,0,0],
+    "C" : [0,0,0,0,0,0,0,0,0,0],
+    "D" : [0,0,0,0,0,0,0,0,0,0],
+    "E" : [0,0,0,0,0,0,0,0,0,0],
+    "F" : [0,0,0,0,0,0,0,0,0,0],   
+    "G" : [0,0,0,0,0,0,0,0,0,0],
+    "H" : [0,0,0,0,0,0,0,0,0,0]
+}
+
 var myShot = [];
 var intTotalBombs = 10
+var notDead = true;
 // Functions
+
+function InitiateGame(){
+    console.log("Welcone to minesweeper.\n");
+    console.log("How many mines would you like to spawn (3-6)? ");
+    
+    // todo #5 write code for accepting input for game initialization
+
+
+    myShot=[A,2];
+}
 
 function CheckTile(shotToCheck){
 
@@ -18,9 +40,13 @@ function CheckNeighbour(shotToChecky){
 }
 
 // And antion!
-myShot=[A,2];
-while (intTotalBombs > 0){
-    CheckTile(myShot)
+
+InitiateGame();
+
+
+while (intTotalBombs > 0 && notDead){
+    CheckTile(myShot);
+    CheckNeighbour(myShot);
 }
 
 
