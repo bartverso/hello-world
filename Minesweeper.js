2771c8fa7c29af013e1fd31f39bb0c2103582895
Bart Verschooren <bart.verschooren@gmail.com>
2021-08-08 23:19:09 +0200

    Minesweeper

---
+++ Minesweeper.js
@@ -0,0 +1,26 @@
+/*  Program:    Minesweeper
+    Developer:  Bart Verschooren
+    Date:       06/08/2021
+*/
+
+// Declarations
+
+var myShot = [];
+var intTotalBombs = 10
+// Functions
+
+function CheckTile(shotToCheck){
+
+}
+
+function CheckNeighbour(shotToChecky){
+
+}
+
+// And antion!
+myShot=[A,2];
+while (intTotalBombs > 0){
+    CheckTile(myShot)
+}
+
+
